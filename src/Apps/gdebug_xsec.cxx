3d5487a21039aebf4ea934802ac111419f9b05d2
Marco Roda <mroda@liverpool.ac.uk>
2019-08-07 14:01:33 +0100

    Full validation

--- src/Apps/gdebug_xsec.cxx
+++ src/Apps/gdebug_xsec.cxx
@@ -34,7 +34,6 @@
 #include "Physics/DeepInelastic/XSection/DISStructureFunc.h"
 #include "Physics/DeepInelastic/XSection/DISStructureFuncModelI.h"
 #include "Framework/Conventions/Units.h"
-#include "Physics/Hadronization/HadronizationModelI.h"
 #include "Framework/Interaction/ProcessInfo.h"
 #include "Framework/Interaction/InitialState.h"
 #include "Framework/Interaction/Interaction.h"
@@ -53,11 +52,7 @@ using namespace genie::utils;
 
 void testRES();
 void testRES2();
-void testDIS();
-void testQelCharm();
-void testCOHGamma() ;
-void testNCgamma();
-
+void testDeltaDecay() ;
 //____________________________________________________________________________
 int main(int argc, char** argv )
 {
@@ -68,119 +63,25 @@ int main(int argc, char** argv )
   }
   RunOpt::Instance()->BuildTune();
 
-  testNCgamma();
+
+  testDeltaDecay() ; 
   return 0;
 }
 
-
 //____________________________________________________________________________
-void testNCgamma()
-{
-  std::ofstream file;
-  file.open("diff_XSec.dat");
-	
+void testDeltaDecay()  {
+
   Messenger * msg = Messenger::Instance();
+  msg -> SetPriorityLevel( "ResonanceDecay", pDEBUG ) ;
+  
 
   AlgFactory * algf = AlgFactory::Instance();
-  const XSecAlgorithmI * xsec_model =
-      dynamic_cast<const XSecAlgorithmI *> (
-             algf->GetAlgorithm("genie::AlvarezRusoCOHPiPXSec", "Default"));
+  const Algorithm * algo = algf -> GetAlgorithm("genie::BaryonResonanceDecayer", "BeforeHadronTransport") ;
 
-
-	  
-//    ProcessInfo  proc_info(kScCoherent, kIntWeakNC);
-// 
-//    InitialState init_state(1000060120,14);
-//    init_state.SetProbeE(1);
-// 
-	
-  double Ev = 1. ;
-  Interaction * interaction = Interaction::COHNC( 1000060120, 14, Ev ) ; 
-
-  Kinematics * kine = interaction -> KinePtr() ;
-   
-   double gamma_th   = 10.0 * 3.14159/180.0;
-   double gamma_phi  = 0.0 * 3.14159/180.0;
-   double lepton_th  = 10.0 * 3.14159/180.0;
-   double lepton_phi = 10.0 * 3.14159/180.0;
-   
-   double lepton_E_max   = Ev;
-   double lepton_E_min = 0.0;
-   double lepton_E = lepton_E_max;
-   double npts = 50;
-   
-   std::cout<<std::endl<<"******************************************************"<<std::endl;
-   for(int i = 0; i < npts; i++) {
-	    
-		
-		double gamma_E = Ev - lepton_E ; 
-		
-		// convert in proper TLorentzVectors
-
-		TLorentzVector lep_p( lepton_E * TMath::Sin( lepton_th ) * TMath::Cos( lepton_phi) , 
-								lepton_E * TMath::Sin( lepton_th ) * TMath::Sin( lepton_phi) , 
-								lepton_E * TMath::Cos( lepton_th ) , 
-								lepton_E ) ;
-								
-		TLorentzVector gamma_p( gamma_E * TMath::Sin( gamma_th ) * TMath::Cos( gamma_phi) , 
-								gamma_E * TMath::Sin( gamma_th ) * TMath::Sin( gamma_phi), 
-								gamma_E * TMath::Cos( gamma_th ) , 
-								gamma_E ) ;
-		
-		
-		kine -> SetFSLeptonP4( lep_p ) ;
-		kine -> SetHadSystP4( gamma_p ) ; 
-		
-		double val = xsec_model -> XSec( interaction, kPSElOlOpifE ) ;
-		double units = 197.33e-16 * 197.33e-16;
-		val *= units;
-		
-		std::cout<<"k0 = " << Ev <<"  ;    theta = "<< lepton_th <<"  ;    theta_g = "<< gamma_th <<"  ;    phi_g = "<< gamma_phi << "  ;    ";
-		std::cout<<"E_gamma = " << gamma_E <<"  ;    XSec = "<<val<<std::endl;
-		file<<gamma_E<<"  "<<val<<std::endl;
-		
-		lepton_E -= (lepton_E_max - lepton_E_min)/double(npts-1);
-		
-   }
-  std::cout<<"******************************************************"<<std::endl<<std::endl;
-  
-  file.close();
-  
-  delete interaction;
 }
 //____________________________________________________________________________
 
-//____________________________________________________________________________
-void testQelCharm()
-{
-  Messenger * msg = Messenger::Instance();
-  msg->SetPriorityLevel("QELCharmXSec", pDEBUG);
 
-  AlgFactory * algf = AlgFactory::Instance();
-  const XSecAlgorithmI * xsec_model =
-      dynamic_cast<const XSecAlgorithmI *> (
-             algf->GetAlgorithm("genie::KovalenkoQELCharmPXSec", "Default"));
-
-
-   ProcessInfo  proc_info(kScQuasiElastic, kIntWeakCC);
-
-   InitialState init_state(1000010010,14);
-   init_state.SetProbeE(5);
-
-   Interaction * interaction = new Interaction(init_state, proc_info);
-
-   Target * target  = interaction->InitStatePtr()->TgtPtr();
-   XclsTag * xcls = interaction->ExclTagPtr();
-
-   target->SetHitNucPdg(kPdgProton);
-   xcls->SetCharm(kPdgSigmaPPc);
-
-   LOG("Main", pNOTICE)
-     << "integral{dxsec/dQ2} = " << xsec_model->Integral(interaction) / (1E-40*units::cm2) << " 1E-40 cm2";;
-
-  delete interaction;
-}
-//____________________________________________________________________________
 void testRES()
 {
   Messenger * msg = Messenger::Instance();
@@ -304,109 +205,3 @@ void testRES2()
   delete interaction;
 }
 //____________________________________________________________________________
-void testDIS()
-{
-  Messenger * msg = Messenger::Instance();
-//  msg->SetPriorityLevel("DISPXSec",  pDEBUG);
-//  msg->SetPriorityLevel("DISXSec",  pDEBUG);
-//  msg->SetPriorityLevel("DISSF",     pDEBUG);
-//  msg->SetPriorityLevel("BodekYang", pDEBUG);
-//  msg->SetPriorityLevel("PDFLIB",    pDEBUG);
-/*
-  double Ev = 10;
-  double Q2 = 2;
-  double x  = 0.7;
-  double y  = Q2 / (2.*x*kNucleonMass*Ev);
-*/
-  double Ev = 80;
-  double x  = 0.5;
-  double y  = 0.5;
-  double Q2 = 2*x*y*Ev*kNucleonMass;
-  double W2 = kNucleonMass2 + 2*Ev*kNucleonMass*y*(1-x);
-  double W  = TMath::Sqrt(TMath::Max(0., W2));
-
-  const int Nq = 8;
-  int  qrk[Nq] = {kPdgUQuark, kPdgUQuark, kPdgAntiUQuark, kPdgDQuark, kPdgDQuark, kPdgAntiDQuark, kPdgSQuark, kPdgAntiSQuark };
-  bool sea[Nq] = {false,      true,       true,           false,      true,       true,           true,       true};
-
-  // -- request the specified DIS SF model
-  AlgFactory * algf = AlgFactory::Instance();
-
-  const XSecAlgorithmI * xsec_cc =
-      dynamic_cast<const XSecAlgorithmI *> (
-                   algf->GetAlgorithm("genie::DISPartonModelPXSec", "CC-Default"));
-
-  PDGLibrary * pdglib = PDGLibrary::Instance();
-
-  double sum=0;
-
-//  Interaction * interaction = Interaction::DISCC(kPdgTgtFe56,kPdgNeutron,kPdgNuMu);
-//  Interaction * interaction = Interaction::DISCC(kPdgTgtFreeN,kPdgNeutron,kPdgNuMu);
-//  Interaction * interaction = Interaction::DISNC(kPdgTgtFreeP,kPdgProton,kPdgNuMu);
-//  Interaction * interaction = Interaction::DISNC(kPdgTgtFe56,kPdgProton,kPdgNuMu);
-//  Interaction * interaction = Interaction::DISNC(kPdgTgtFreeP,kPdgProton,kPdgNuMu);
-//  Interaction * interaction = Interaction::DISCC(kPdgTgtFreeN,kPdgNeutron,kPdgAntiNuMu);
-//  Interaction * interaction = Interaction::DISNC(kPdgTgtFreeN,kPdgNeutron,kPdgNuMu);
-
-//HERE
-  Interaction * interaction = Interaction::DISNC(kPdgTgtFreeP,kPdgProton,kPdgAntiNuMu);
-
-  interaction->InitStatePtr()->SetProbeE(Ev);
-
-  Kinematics * kine = interaction->KinePtr();
-  kine->Setx(x);
-  kine->Sety(y);
-  kine->SetQ2(Q2);
-  kine->SetW(W);
-
-  LOG("Main", pNOTICE) << *interaction;
-
-  //interaction->SetBit(kISkipProcessChk);
-  //interaction->SetBit(kISkipKinematicChk);
-
-  Interaction * interaction2 = new Interaction(*interaction);
-
-/*
-  LOG("Main", pNOTICE) << "d2xsec/dxdy [vN] = " << xsec_cc->XSec(interaction, kPSxyfE) / units::cm2;
-  sum=0;
-  for(int iq=0; iq<Nq; iq++) {
-      LOG("Main", pNOTICE) << " **** USING: " << pdglib->Find(qrk[iq])->GetName() << ", sea:" << sea[iq] << "]";
-      interaction2->InitStatePtr()->TgtPtr()->SetHitQrkPdg(qrk[iq]);
-      interaction2->InitStatePtr()->TgtPtr()->SetHitSeaQrk(sea[iq]);
-      //LOG("Main", pNOTICE) << *interaction;
-      double xsec   = xsec_cc->XSec(interaction2, kPSxyfE) / units::cm2;
-      LOG("Main", pNOTICE) << "d2xsec/dxdy [q:" << pdglib->Find(qrk[iq])->GetName()
-                           << ", sea:" << sea[iq] << "]= " << xsec;
-      sum+=xsec;
-  }
-  LOG("Main", pNOTICE) << "d2xsec/dxdy [SUM]= " << sum;
-*/
-
-  msg->SetPriorityLevel("DISXSec",   pNOTICE);
-  msg->SetPriorityLevel("DISPXSec",  pNOTICE);
-  msg->SetPriorityLevel("DISSF",     pNOTICE);
-  msg->SetPriorityLevel("BodekYang", pNOTICE);
-  msg->SetPriorityLevel("PDFLIB",    pNOTICE);
-
- interaction ->SetBit(kINoNuclearCorrection);
- interaction2->SetBit(kINoNuclearCorrection);
-
-  LOG("Main", pNOTICE) << "Integral{d2xsec/dxdy [vN]} = " << xsec_cc->Integral(interaction) / units::cm2;
-  sum=0;
-  for(int iq=0; iq<Nq; iq++) {
-      LOG("Main", pNOTICE) << " **** USING: " << pdglib->Find(qrk[iq])->GetName() << ", sea:" << sea[iq] << "]";
-      interaction2->InitStatePtr()->TgtPtr()->SetHitQrkPdg(qrk[iq]);
-      interaction2->InitStatePtr()->TgtPtr()->SetHitSeaQrk(sea[iq]);
-      //LOG("Main", pNOTICE) << *interaction2;
-      double xsec =  xsec_cc->Integral(interaction2) / units::cm2;
-      LOG("Main", pNOTICE) << "integral{d2xsec/dxdy [q:" << pdglib->Find(qrk[iq])->GetName()
-                           << ", sea:" << sea[iq] << "]}= " << xsec;
-      sum+=xsec;
-  }
-  LOG("Main", pNOTICE) << "integral{d2xsec/dxdy} [SUM]= " << sum;
-
-  delete interaction;
-  delete interaction2;
-}
-//____________________________________________________________________________
-
